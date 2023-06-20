2fd8b2adce7b5cc0604a49d4567215b60a87520e
Takuma Yamashita <c0b22154ba@edu.teu.ac.jp>
2023-06-20 15:20:59 +0900

    練習３

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -19,17 +19,30 @@ def main():
     y = random.randint(0, HEIGHT)  # 爆弾用の乱数、y軸
     bb_rct = bb_img.get_rect()
     bb_rct.center = x, y  # 練習２
-    vx, vy = +5, +5
+    vx, vy = +5, +5  #練習２
+    moving = {pg.K_UP: (0, -5), pg.K_DOWN: (0, +5), pg.K_LEFT: (-5, 0), pg.K_RIGHT: (+5, 0)}
+    kk_rct = kk_img.get_rect()
     clock = pg.time.Clock()
     tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0, 0]
+ 
+        for k, mv in moving.items():
+            if key_lst[k]:
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
+
+        kk_rct[0] += sum_mv[0]
+        kk_rct[1] += sum_mv[1]
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
-        bb_rct.move_ip(vx, vy)
+        screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx, vy)  #  練習２
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
